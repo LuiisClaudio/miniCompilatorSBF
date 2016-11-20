be6a389afa3c57c9bd8041c38008df3c469883fc
LuiisClaudio <luisclaudio97@hotmail.com>
2016-11-20 21:28:36 -0200

    Primeira main

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "geracod.h"
+
+int main (void) {
+  int i;
+  void **code;
+  FILE *myfp;
+  funcp entry;
+  
+
+  if ((myfp = fopen("//home//luis//tra2//ex1.txt", "r")) == NULL)
+  {
+    perror ("nao conseguiu abrir arquivo!");
+    exit(1);
+  }
+  //void geracod (FILE *f, void **code, funcp *entry);
+  geracod(myfp, code, &entry);
+  entry = (funcp)code;
+
+  i = (*entry)(0);
+  printf("\ni = %d\n", i);
+  
+  return 0;
+}
