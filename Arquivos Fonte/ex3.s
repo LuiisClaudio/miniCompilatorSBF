19e3123ce2ed9fd3cd17a84ddff935e7e64ea5df
lucasferraco <lucasferracof@gmail.com>
2016-11-23 16:44:26 -0200

    Conserto de erros em ex3.s

--- Arquivos Fonte/ex3.s
+++ Arquivos Fonte/ex3.s
@@ -1,11 +1,13 @@
-/*function
-ret? p0 $1
-v0 = p0 + $0
-v1 = v0 - $1
-v1 = call 0 v1
-v0 = v0 * v1
-ret? $0 v0
-end*/
+/*
+ function
+ ret? p0 $1
+ v0 = p0 + $0
+ v1 = v0 - $1
+ v1 = call 0 v1
+ v0 = v0 * v1
+ ret? $0 v0
+ end
+*/
 
 .data
 .text
@@ -16,23 +18,22 @@ ex3:
 f0:
 push %rbp
 movq %rsp, %rbp
-sub $32, %rsp
+sub $16, %rsp
 movl $0, %ebx
 
 #ret? p0 $1
-movl $1, %eax /*  */
+movl $1, %eax
 cmpl $0, %edi /* p0 == 0 ? */
 je FIM
 movl $0, %eax
 
 #v0 = p0 + $0
-movl %edi, %r12d /* v0 = p0 */
-add $0, %r12d
-movl %r12d, -4(%rbp) /* guarda v0 na pilha */
+movl %edi, -4(%rbp) /* v0 = p0, guarda v0 na pilha */
 
 #v1 = v0 - $1
 movl -4(%rbp), %r12d /* v1 = v0 */
 subl $1, %r12d /* v1 = v0 - 1 */
+movl %r12d, -8(%rbp) /* guarda v1 na pilha em cima de v0 */
 
 #v1 = call 0 v1
 movl %r12d, %edi
