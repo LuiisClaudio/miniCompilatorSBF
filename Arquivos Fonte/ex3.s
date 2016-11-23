479e5c0dceb083e8c14d697e5f83ec0d01b3b6c1
lucasferraco <lucasferracof@gmail.com>
2016-11-23 15:53:21 -0200

    ex3.s completo

--- Arquivos Fonte/ex3.s
+++ Arquivos Fonte/ex3.s
@@ -16,45 +16,37 @@ ex3:
 f0:
 push %rbp
 movq %rsp, %rbp
-sub $20, %rsp
+sub $32, %rsp
 movl $0, %ebx
 
 #ret? p0 $1
-movl $0, %ebx
-movl $1, %eax
-cmpl $0, %ebx
+movl $1, %eax /*  */
+cmpl $0, %edi /* p0 == 0 ? */
 je FIM
 movl $0, %eax
 
 #v0 = p0 + $0
-movl %edi, %r12d
-movl $1, %r13d
-add %r12d, %r13d
-movl %r13d, -4(%rbp)
+movl %edi, %r12d /* v0 = p0 */
+add $0, %r12d
+movl %r12d, -4(%rbp) /* guarda v0 na pilha */
 
 #v1 = v0 - $1
-movl -4(%rbp), %r12d
-movl $1, %r13d
-subl %r13d, %r12d 
+movl -4(%rbp), %r12d /* v1 = v0 */
+subl $1, %r12d /* v1 = v0 - 1 */
 
 #v1 = call 0 v1
-movl $0, %edi
-jmp f0
-
+movl %r12d, %edi
+call f0
+movl %eax, -8(%rbp) /* guarda v1 na pilha em cima de v0 */
 
 #v0 = v0 * v1
 movl -4(%rbp), %r12d
 movl -8(%rbp), %r13d
-imull %r12d, %r13d
+imull %r13d, %r12d /* v0 = v0 * v1 */
 movl %r13d, -4(%rbp)
 
 #ret? $0 v0
-movl $0, %ebx
 movl -4(%rbp), %eax
-
-cmpl $0, %ebx
-je FIM
-movl $0, %eax
 
 FIM:
 movq %rbp, %rsp
